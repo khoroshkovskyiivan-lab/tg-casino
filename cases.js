a170ea37d6960335260195aba8e6c21c00ff0fa0
khoroshkovskyiivan-lab <khoroshkovskyiivan@gmail.com>
2026-01-20 18:33:31 +0100

    Update and rename cases.js.txt to cases.js

---
+++ cases.js
@@ -0,0 +1,13 @@
+const cases = [
+    {
+        id: "basic",
+        name: "Обычный кейс",
+        price: 100,
+        drops: [
+            { stars: 50, chance: 60 },
+            { stars: 100, chance: 25 },
+            { stars: 300, chance: 10 },
+            { stars: 1000, chance: 5 }
+        ]
+    }
+];
